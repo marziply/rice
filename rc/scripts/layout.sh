768423d044c291da67c90e890548d0cf6b772f5c
Hayden Perry <haydencwperry@gmail.com>
2020-06-09 22:56:57 +0100

    Updated layout script

--- rc/scripts/layout.sh
+++ rc/scripts/layout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --primary --mode 2560x1440 --pos 0x325 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off --output DVI-D-0 --off
+xrandr --output DisplayPort-1 --primary --mode 2560x1440 --pos 0x339 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off --output DVI-D-0 --off
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
