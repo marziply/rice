769f049b843030b5de65994845647a5ee314320f
Hayden Perry <haydencwperry@gmail.com>
2020-11-04 20:29:56 +0000

    Added Passphrase field to lpass rofi script

--- rofi/scripts/lpass.sh
+++ rofi/scripts/lpass.sh
@@ -17,6 +17,12 @@ done
 
 sel=`printf "%s\n" "${!map[@]}" | rofi -dmenu -i -p "Account" -width 900`
 
-if [ "$sel" != "" ]; then
-  lpass show -c --password ${map[$sel]}
+if [ -n $sel ]; then
+  pass=$(lpass show --password ${map[$sel]})
+
+  if [ -z $pass ]; then
+    pass=$(lpass show --field=Passphrase ${map[$sel]})
+  fi
+
+  echo $pass | tr -d "\n" | xclip -sel clip
 fi
