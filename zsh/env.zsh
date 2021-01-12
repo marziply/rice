679e2df70dd519181ca3706595c951af1dba0156
Hayden Perry <haydencwperry@gmail.com>
2021-01-12 14:41:04 +0000

    Replaced prefix key for tmux

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,6 +2,8 @@
 
 export GTK_THEME="Aritim-Dark"
 
+export PYTHONPATH="/usr/lib/python3.9"
+
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
