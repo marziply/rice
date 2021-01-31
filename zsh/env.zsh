417a5a4463f031284ee4bf6b2011bead7b74acf5
Hayden Perry <haydencwperry@gmail.com>
2021-01-31 00:12:14 +0000

    vimrc updates

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
+export BROWSER="brave --enable-features=UseOzonePlatform --ozone-platform=wayland"
+export EDITOR="nvim"
+export TERMINAL="alacritty"
+export READER="zathura"
+
+export GTK_THEME="Aritim-Dark"
+
 if [[ $TERM == "tmux" ]]; then
   export GEM_HOME="$HOME/.rvm/gems/ruby-3.0.0"
   export GEM_PATH="$GEM_HOME/bin"
@@ -13,17 +20,10 @@ export LOCAL_BIN="$HOME/.local/bin"
 export PATH="$PATH:$LOCAL_BIN:$RVM_BIN"
 export PYTHONPATH="/usr/lib/python3.9"
 
-export GTK_THEME="Aritim-Dark"
-
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_RUNTIME_DIR="/run/user/1000"
-
-export BROWSER="brave --enable-features=UseOzonePlatform --ozone-platform=wayland"
-export EDITOR="nvim"
-export TERMINAL="alacritty"
-export READER="zathura"
 
 export NVIM_DIR="$XDG_CONFIG_HOME/nvim"
 export NVIM_COC_LOG_FILE="$XDG_RUNTIME_DIR/coc.log"
