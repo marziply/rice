5a6ac97a174c0e2042e8d1dc1ba6a72384e00254
marziply <contact@marziply.dev>
2021-07-29 15:01:03 +0100

    Moved aliases to its own script

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -94,35 +94,15 @@ startc() {
     $1
 }
 
-alias pgc="clear && pgcli postgres postgres"
-alias pm="sudo pacman -S"
-alias pms="sudo pacman -Ss"
-alias pmr="sudo pacman -Rvns"
-alias ym="yay -S"
-alias yms="yay -Ss"
-alias sc="sudo systemctl"
-alias scu="systemctl --user"
-alias scs="sc start"
-alias sct="sc status"
-alias scou="scu stop"
-alias scsu="scu start"
-alias sctu="scu status"
-alias scou="scu stop"
-alias gacam="ga . && gcam"
-alias ggpnv="ggpush --no-verify"
-alias stopc='sudo docker container stop $(sudo docker container ls -lq)'
-alias sshk="ssh rancher@k3s.home.dev"
-alias kc="kubectl"
-alias kcg="kc get"
-alias kcgp="kcg pods"
-alias kcga="kcg all"
-alias kcgaa="kcg all --all-namespaces"
-alias kcd="kc delete"
-alias kcdp="kcd pod"
-alias kcdj="kcd job"
-
+source "$XDG_CONFIG_HOME/zsh/aliases.zsh"
 source "$ZSH/oh-my-zsh.sh"
 source "$HOME/.cargo/env"
 source "$HOME/.rvm/scripts/rvm"
 
 rm -rf "$HOME/.zcompdump"
+rm -rf \
+  "$HOME/Documents" \
+  "$HOME/Downloads" \
+  "$HOME/Music" \
+  "$HOME/Videos" \
+  "$HOME/Pictures"
