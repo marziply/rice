4029f247116e44fa0d7785b4a2679829410028b1
marziply <contact@marziply.dev>
2022-06-16 13:47:48 +0100

    Renamed ops directory to rocinante

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -7,7 +7,7 @@ default_marks=(
   "config : $CONFIG_DIR"
   "projects : $HOME/dev/projects"
   "work : $HOME/dev/work"
-  "ops : $HOME/dev/ops"
+  "rocinante : $HOME/dev/rocinante"
 )
 
 start_sway() {
