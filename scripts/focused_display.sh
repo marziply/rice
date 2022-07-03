90c53eb2ecb7bd41f71cd02954d91add5562a376
marziply <contact@marziply.dev>
2022-07-03 20:56:24 +0100

    Renamed and moved around vars in focused display script

--- scripts/focused_display.sh
+++ scripts/focused_display.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-all_workspaces=$(swaymsg -rt get_workspaces)
-focused_w=$(echo $all_workspaces | jq -c '.[] | select(.focused)')
-
 monitors=$(\
   xrandr --listmonitors \
   | awk '/\s[0-2]/ { gsub("(/[0-9]+|)(x|+)", ":", $3); print $3, $4 }'
 )
-focused_m=$(\
+workspace=$(\
+  swaymsg -rt get_workspaces \
+  | jq -c '.[] | select(.focused)'
+)
+display=$(\
   swaymsg -rt get_outputs \
   | jq '.[] | select(.focused).rect | .width, .height, .x, .y | tostring' \
   | jq -rs 'join(":")'
@@ -16,13 +17,13 @@ focused_m=$(\
 while [ -n "$1" ]; do
   case "$1" in
     index)
-      echo $focused_w | jq '.num'
+      echo $workspace | jq '.num'
     ;;
     name)
-      echo $focused_w | jq -r '.name'
+      echo $workspace | jq -r '.name'
     ;;
     monitor)
-      echo "${monitors[@]}" | awk -v F=$focused_m '$1 == F { print $NF }'
+      echo "${monitors[@]}" | awk -v F=$display '$1 == F { print $NF }'
     ;;
   esac
 
