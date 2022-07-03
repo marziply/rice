76b589f7187f4d5825b7353cc9fc24c6394d88ad
marziply <contact@marziply.dev>
2022-07-03 18:52:53 +0100

    Fixed monitor focused script

--- scripts/focused_display.sh
+++ scripts/focused_display.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-outputs=$(swaymsg -rt get_workspaces)
-focused=$(echo $outputs | jq -c '.[] | select(.focused)')
+all_workspaces=$(swaymsg -rt get_workspaces)
+focused_w=$(echo $all_workspaces | jq -c '.[] | select(.focused)')
 
-index() {
-  echo $focused | jq '.output[-1:] | tonumber -1'
-}
+monitors=$(\
+  xrandr --listmonitors \
+  | awk '/\s[0-2]/ { gsub("(/[0-9]+|)(x|+)", ":", $3); print $3, $4 }'
+)
+focused_m=$(\
+  swaymsg -rt get_outputs \
+  | jq '.[] | select(.focused).rect | .width, .height, .x, .y | tostring' \
+  | jq -rs 'join(":")'
+)
 
 while [ -n "$1" ]; do
   case "$1" in
     index)
-      index
+      echo $focused_w | jq '.num'
     ;;
     name)
-      echo $focused | jq -r '.name'
+      echo $focused_w | jq -r '.name'
     ;;
     monitor)
-      echo "XWAYLAND$(index)"
+      echo "${monitors[@]}" | awk -v F=$focused_m '$1 == F { print $NF }'
     ;;
   esac
 
