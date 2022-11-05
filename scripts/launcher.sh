ec10e5857e7f94dae140adcf25951851306115e9
marziply <contact@marziply.dev>
2022-11-05 11:05:13 +0000

    Vimrc updates and fixed launcher script

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -2,8 +2,7 @@
 
 source $ZSH_DIR/env.zsh
 
+displays=($(xrandr --listmonitors | grep -Po '(?<=\+)XWAYLAND\d+'))
 monitor=$(swaymsg -rt get_outputs | jq 'map(.focused) | index(true)')
-# monitor=$(swaymsg -rt get_tree | jq 'recurse(.nodes[]; .focused)')
 
-# rofi -show drun -m "$($SCRIPTS_DIR/focused_display.sh monitor)" $@
-rofi -show drun -m "XWAYLAND${monitor}" $@
+rofi -show drun -m "${displays[monitor]}" $@
