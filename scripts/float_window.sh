e365fe1c1d0b4a0351d1b6358848dfc693da7982
marziply <contact@marziply.dev>
2022-11-22 09:27:55 +0000

    Deleted unused scripts

--- scripts/float_window.sh
+++
@@ -1,7 +0,0 @@
-win=$(i3-msg -t get_tree | jq 'recurse(.nodes[]) | select(.focused == true) | .')
-
-if [[ -z $win ]]; then
-  i3-msg floating disable, focus up, kill window
-else
-  i3-msg splitv open, focus up, floating enable
-fi
